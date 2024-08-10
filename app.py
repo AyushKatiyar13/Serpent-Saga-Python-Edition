6f0ba4656bccd43b79f72bd287ac0bcdf75641d4
Ayush Katiyar <ayushkatiyar1301@gmail.com>
2024-08-10 21:56:13 +0530

    integrated flask

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, send_file
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # This is just a placeholder. You can modify it to start your game or provide instructions.
+    return 'Welcome to the Snake Game! <a href="/game">Play the Game</a>'
+
+@app.route('/game')
+def game():
+    # Assuming you want to provide a download link for the game script
+    return send_file('Snake game.py', as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
